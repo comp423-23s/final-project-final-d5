4664681257bef90a5f844017b104f0f44e6476dd
Will Astilla <77816687+wastilla@users.noreply.github.com>
2023-03-26 16:07:45 -0400

    Created post data model (#12)
    Add Post datamodel

---
+++ backend/models/post.py
@@ -0,0 +1,13 @@
+"""Post model serves as data model for posts in the Forum"""
+
+from pydantic import BaseModel
+from . import User
+
+
+class Post(BaseModel):
+    id: int | None = None
+    content: str
+    user: User
+    votes: list[User]
+    timestamp: str
+
