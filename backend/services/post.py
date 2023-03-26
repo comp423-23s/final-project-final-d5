23ee67a6dca099b3e83daa070c79c4e4339b79e2
wastilla <wastilla@email.unc.edu>
2023-03-26 16:54:55 -0400

    Implement create method in post service

--- backend/services/post.py
+++ backend/services/post.py
@@ -2,7 +2,7 @@ from fastapi import Depends
 from sqlalchemy import select, or_, func
 from sqlalchemy.orm import Session
 from ..database import db_session
-from ..models import User, Role, RoleDetails, Permission
+from ..models import User, Role, RoleDetails, Permission, Post
 from ..entities import RoleEntity, PermissionEntity, UserEntity
 from .permission import PermissionService, UserPermissionError
 
@@ -11,3 +11,12 @@ class PostService:
     def __init__(self, session: Session = Depends(db_session), permission: PermissionService = Depends()):
         self._session = session
         self._permission = permission
+
+    def create(self, subject: User, post: Post) -> User:
+    
+        # if subject != user:
+        #     self._permission.enforce(subject, 'user.create', 'user/')
+        entity = PostEntity.from_model(post)
+        self._session.add(entity)
+        self._session.commit()
+        return entity.to_model()
