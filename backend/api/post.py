8045c8e3ea98fd850887d3b7ddeb9397a561d964
Will Astilla <77816687+wastilla@users.noreply.github.com>
2023-03-30 17:57:07 -0400

    Add finishing touches to Views as well as error handling
    Add styling to HTML elements and navigation between forum pages.
    Implement error handling for post creation

--- backend/api/post.py
+++ backend/api/post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from ..services import PostService, UserService
 from ..models import Post
 from .authentication import registered_user
@@ -8,7 +8,10 @@ api = APIRouter(prefix="/api/post")
 
 @api.post("", response_model=Post, tags=['Post'])
 def create(post: Post, post_svc: PostService = Depends(), usr_svc: UserService = Depends()):
-    user_entity = usr_svc.findUser(post.user)
+    try:
+        user_entity = usr_svc.findUser(post.user)
+    except:
+        raise HTTPException(status_code=422, detail=str("User Not Registered"))
     return post_svc.create(post,user_entity)
 
 @api.get("", response_model=list[Post], tags=['Post'])
