b3f6d5b825f8580fa158fc7f7622276e131d90b9
Will Astilla <77816687+wastilla@users.noreply.github.com>
2023-04-16 15:12:12 -0400

    Paginate posts in forum (#53)
    Paginate forum results with in-built function. Update number of pages when pages are added/deleted

--- frontend/src/app/viewforum/viewforum.component.ts
+++ frontend/src/app/viewforum/viewforum.component.ts
@@ -3,6 +3,9 @@ import { Post, PostService } from '../post.service';
 import { Observable } from 'rxjs';
 import { PermissionService } from '../permission.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { MatPaginator } from '@angular/material/paginator';
+import { map } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-getforum',
@@ -12,7 +15,12 @@ import { HttpErrorResponse } from '@angular/common/http';
 export class viewforumComponent {
   public adminPermission$: Observable<boolean>;
   public post$: Observable<Post[]>;
-  
+  public displayPost$: Observable<Post[]>;
+  public itemsPerPage: number = 5; // set the number of items to display per page
+  public currentPage: number = 1;
+  public numPosts: number;
+  public numPages: number;
+
   public static Route = {
     path: 'viewforum',
     component: viewforumComponent
@@ -22,7 +30,19 @@ export class viewforumComponent {
     private postService: PostService,
     private permission: PermissionService,
     ) {
+    this.numPosts = 0; //numPosts and numPages must be set to zero initially
+    this.numPages = 0;
     this.post$ = postService.getPosts()
+    this.displayPost$ = this.post$.pipe( //paginating total number of posts
+      map((items) => {
+        const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+        return items.slice(startIndex, startIndex + this.itemsPerPage);
+      }))
+    this.post$.subscribe((items) => { //set number of pages and number of total posts
+      this.numPosts = items.length;
+      this.numPages = Math.ceil(items.length / this.itemsPerPage)
+    });
+    
     this.adminPermission$ = this.permission.check('admin.view', 'admin/')
   }
 
@@ -37,6 +57,15 @@ export class viewforumComponent {
 
   private onSuccess(): void { // get new posts after deletion
     this.post$ = this.postService.getPosts()
+    this.displayPost$ = this.post$.pipe(
+      map((items) => {
+        const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+        return items.slice(startIndex, startIndex + this.itemsPerPage);
+      }))
+      this.post$.subscribe((items) => { //update number of pages after deletion
+        this.numPosts = items.length;
+        this.numPages = Math.ceil(items.length / this.itemsPerPage)
+      });
   }
 
   private onError(err: HttpErrorResponse) {
@@ -45,6 +74,19 @@ export class viewforumComponent {
     } else {
       console.log(JSON.stringify(err)); // see what error is
     }
+  }
+
+  public getCurrentPage(pageNumber: number): Observable<any[]> {
+    return this.displayPost$
+  }
+
+  updateItems() { //updating displayedPosts based on what page is currently selected
+    this.displayPost$ = this.post$.pipe(
+      map((items) => {
+        const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+        return items.slice(startIndex, startIndex + this.itemsPerPage);
+      })
+    );
   }
 
 }
