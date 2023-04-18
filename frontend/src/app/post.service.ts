c743a84fbcc75f3fd68d802ea64ba27f3004ae91
wquadland <59710771+wquadland@users.noreply.github.com>
2023-04-18 16:02:17 -0400

    Added warnings for title and content

--- frontend/src/app/post.service.ts
+++ frontend/src/app/post.service.ts
@@ -64,11 +64,11 @@ export class PostService {
 
     makePost(id: number, title: string, content: string, user: Profile, votes: [], timestamp: string): Observable<Post> {
 
-        if (title.length == 0) {
-            return throwError(() => new Error("Unable to Post empty title, please check your input!"));
-        }
-
-        if (content.length == 0) {
+        if ((title.length == 0) && (content.length == 0)) {
+            return throwError(() => new Error("Unable to post empty title and empty content, please check your input!"));
+        } else if (title.length == 0) {
+            return throwError(() => new Error("Unable to post empty title, please check your input!"));
+        } else if (content.length == 0) {
             return throwError(() => new Error("Unable to Post empty content, please check your input!"));
         }
 
