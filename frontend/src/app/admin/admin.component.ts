af4c9107a0cba5341288bd8a37cc23cbc868aba9
angelinasu57 <angelinasu57@gmail.com>
2023-04-26 09:43:00 -0400

    Approved posts showing, fixed formatting to match forum.

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Profile, ProfileService } from '../profile/profile.service';
+import { Post, PostService } from '../post.service';
+import { observeNotification } from 'rxjs/internal/Notification';
 
 @Component({
   selector: 'app-admin',
@@ -10,6 +12,7 @@ import { Profile, ProfileService } from '../profile/profile.service';
 export class AdminComponent implements OnInit {
 
   public profile$: Observable<Profile | undefined>;
+  public post$: Observable<Post[]>;
 
   public links = [
     { label: 'Users', path: '/admin/users' },
@@ -18,10 +21,16 @@ export class AdminComponent implements OnInit {
 
   constructor(
     public profileService: ProfileService,
+    private postService: PostService,
     ) {
     this.profile$ = profileService.profile$;
+    this.post$ = postService.getPosts()
   }
 
   ngOnInit(): void {}
+
+  public showApproved(): Observable<any[]> {
+    return this.post$
+  }
 
 }
